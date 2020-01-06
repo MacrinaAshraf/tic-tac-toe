aa8a5aea89ae1baabac20e827ffbc8e86b75f866
Adham <mohamedadham@gmail.com>
2020-01-06 20:36:03 +0200

    Adding Invite and response to invite functions

--- Server/src/chatserver/ChatHandler.java
+++ Server/src/chatserver/ChatHandler.java
@@ -9,6 +9,8 @@ import java.util.Hashtable;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 class ChatHandler extends Thread{
 	Client client;
@@ -37,10 +39,18 @@ class ChatHandler extends Thread{
     public void run(){ 
     	
     	String str;
+        JSONObject message;
     	try {
 		   //first input is the username
 		   str = dis.readLine(); 
-		   //set the username of the object
+                   message=new JSONObject(str);
+                   String type =(String)message.get("type");
+                   if(type=="invite")
+                       invite(message);
+                   else if(type=="responsetoinvite")
+                       respondToInvite(message);
+                  
+                    //set the username of the object
 		   client.setUserName(str);
 		   //set the username as key and its stream as value in dictionary
 		   streams.put(client.getUserName(), client.getPrintStream());                    
@@ -50,7 +60,9 @@ class ChatHandler extends Thread{
 		   client.setPlayingWith(str);               
 	   } catch (IOException ex) {
 		   Logger.getLogger(ChatHandler.class.getName()).log(Level.SEVERE, null, ex);
-	   }
+	   } catch (JSONException ex) {
+                Logger.getLogger(ChatHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
     	//listen from the third input to the end 
     	while (true) {
     		try {
@@ -69,5 +81,46 @@ class ChatHandler extends Thread{
 		opponentPS.println(msg);
 		client.getPrintStream().println(msg);
 	}
+        public void invite(JSONObject msg) throws JSONException{
+          //get the stream of the one to ask to play via the json Object toPlayWith key
+            PrintStream playerToPlayWithStream=(PrintStream) streams.get( msg.get("toPlayWith"));
+	  //write in the player's stream and his/her opponent's stream
+            JSONObject inviteMessage=new JSONObject();
+           //setting mesage type key and the username of the asking player
+            inviteMessage.put("type", "invite");
+            inviteMessage.put("username", msg.get("askingplayername"));
+            //sendint to the player the invitation object
+            playerToPlayWithStream.println(inviteMessage.toString());
+        }
+        public void respondToInvite(JSONObject msg) throws JSONException{
+            //getting the stream of the player to respond to
+            PrintStream opponentPS=(PrintStream) streams.get( msg.get("toPlayWith"));
+            JSONObject responseToInviteMessage=new JSONObject();
+            //setting the type of the JSON Object and username of the player who accepted/rejected the invitation
+            responseToInviteMessage.put("type", "responsetoinvite");
+            responseToInviteMessage.put("username", msg.get("username"));
+            if(msg.get("response")=="accept"){ 
+                //setting connections
+                String username=(String)msg.get("username");
+                String toPlayWith=(String)msg.get("toPlayWith");
+                for(int i=0;i<clientsVector.size();i++){
+                    if(clientsVector.elementAt(i).getUserName()== msg.get("toPlayWith")){
+                        clientsVector.elementAt(i).setPlayingWith(username);
+                    }
+                    else if(clientsVector.elementAt(i).getUserName()== msg.get("username")){
+                        clientsVector.elementAt(i).setPlayingWith(toPlayWith);
+                    }
+                }
+                responseToInviteMessage.put("response", "accept");
+                
+            }
+            else {
+                responseToInviteMessage.put("response", "rejected");
+                
+            }
+            //sending the response JSON object to the askingPlayer
+            opponentPS.println(responseToInviteMessage.toString());
+        }
+        
     
 }
--- Server/src/chatserver/ChatServer.java
+++ Server/src/chatserver/ChatServer.java
@@ -10,6 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import org.json.JSONObject;
 
 public class ChatServer {
     ServerSocket myServerSocket = null;
