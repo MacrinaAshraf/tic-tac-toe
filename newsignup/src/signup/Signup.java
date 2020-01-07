4ba31de2e3fd68768b094d8dfb737c59dddd4459
AliIsmail <aliismail@localhost.localdomain>
2020-01-07 13:50:42 +0200

    new signup

---
+++ newsignup/src/signup/Signup.java
@@ -0,0 +1,11 @@
+package signup;
+public class Signup {
+static GUIframe x = new GUIframe();
+public Signup()
+{
+    x.setVisible(true);
+}
+    public static void main(String[] args) {
+        Signup gui = new Signup();
+    }
+}
