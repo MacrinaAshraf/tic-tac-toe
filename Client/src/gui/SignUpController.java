0cf3e7289c0805366ac3c0626f638c10903ce8f8
Nouran-yehia <53382731+Nouran-yehia@users.noreply.github.com>
2020-01-18 04:39:53 -0800

    updating gui for SignUp (adding retype Password)

--- Client/src/gui/SignUpController.java
+++ Client/src/gui/SignUpController.java
@@ -18,6 +18,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import jdk.nashorn.internal.runtime.JSONFunctions;
 import org.json.JSONObject;
@@ -34,6 +35,8 @@ public class SignUpController implements Initializable {
 
 	private Parent homePageUI;
 	private HomePageController homePageControl;
+    @FXML
+    private TextField username;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
